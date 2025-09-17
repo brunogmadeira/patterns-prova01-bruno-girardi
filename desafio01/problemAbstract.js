5460abb11da5c9977cb79b7522b149e85fe7f589
Bruno Girardi Madeira <bruno.57923@alunosatc.edu.br>
2025-09-17 19:33:30 -0300

    Add commit prova

--- desafio01/problemAbstract.js
+++ desafio01/problemAbstract.js
@@ -1,1 +1,76 @@
-//TODO
+// ----- Produtos Abstratos -----
+class Button {
+  render() {}
+}
+
+class Window {
+  render() {}
+}
+
+// ----- Produtos Concretos -----
+class LightButton extends Button {
+  render() {
+    return "Botão branco criado";
+  }
+}
+
+class DarkButton extends Button {
+  render() {
+    return "Botão preto criado";
+  }
+}
+
+class LightWindow extends Window {
+  render() {
+    return "Janela clara aberta";
+  }
+}
+
+class DarkWindow extends Window {
+  render() {
+    return "Janela escura aberta";
+  }
+}
+
+class UIFactory {
+  createButton() {}
+  createWindow() {}
+}
+
+class LightFactory extends UIFactory {
+  createButton() {
+    return new LightButton();
+  }
+  createWindow() {
+    return new LightWindow();
+  }
+}
+
+class DarkFactory extends UIFactory {
+  createButton() {
+    return new DarkButton();
+  }
+  createWindow() {
+    return new DarkWindow();
+  }
+}
+
+function app(factory) {
+  const button = factory.createButton();
+  const windowUI = factory.createWindow();
+
+  console.log(button.render());
+  console.log(windowUI.render());
+}
+
+// ----- Uso -----
+const theme = "dark";
+let factory;
+
+if (theme === "light") {
+  factory = new LightFactory();
+} else {
+  factory = new DarkFactory();
+}
+
+app(factory);
